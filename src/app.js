b0ce66d911e602c74bcad89a26621eada9a89d21
bayhacioglu <hodjaa@gmail.com>
2021-10-19 21:45:52 +0300

    18.10.2021 JS Camp

---
+++ src/app.js
@@ -0,0 +1,20 @@
+console.log("lol")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+const euroDun = 11.2
+
+
+console.log(euroDun)
+//array
+let konutKredileri = ["konut kredisi","emlak kredisi","villa kredisi"]
+
+console.log("<ul>")
+    for (let i = 0; i < konutKredileri.length; i++) {
+        console.log("<li>"+konutKredileri[i]+"</li>")
+        
+    }
+console.log("</ul>")
+
+console.log(konutKredileri)
