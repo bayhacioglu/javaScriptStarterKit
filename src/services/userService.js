c5dba87db1a38458693fa35eb599392b4fe62fac
bayhacioglu <hodjaa@gmail.com>
2021-11-22 21:07:07 +0300

    kamp

--- src/services/userService.js
+++ src/services/userService.js
@@ -18,11 +18,7 @@ export default class UserService{
     }
 
     getById(id){
-<<<<<<< HEAD
-        return this.users.find(u=>u.id===id)
-=======
         return this.users.find(u=>u.id ===id)
->>>>>>> 356709dd1c8d89a8fd8b4505618f36d28c557a25
     }
 
 }
